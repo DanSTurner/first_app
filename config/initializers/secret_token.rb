6b31484b4140549eb10c4a98546a7730c3655a96
Dan S Turner <turner@nowwithmoredan.com>
2013-12-11 14:24:44 -0800

    Restarting tutorial

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = 'b02da62a578431fb10b7ee1cf1ca63012e3fb92d7d2f24d8b8b26554e2622550fb27930a74367d24b896e38e96aaa3d9315f05ee1218ae4280e4150dbfa3b235'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'e0ecb4e76e30c63c9899d465c20e5e6ecaae74b5cca789f69c16a84a938e9d8d53e179b244663f5fa6a2f4df58ff4d10e85281283b2c9b74097c2ba0785d09f6'
